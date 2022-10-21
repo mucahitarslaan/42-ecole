69d0c70845760063edf33263d6a894eba9e77234
mucahitarslaan <mucahitarslaan@gmail.com>
2022-10-21 23:51:07 +0300

    update

---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <stdio.h>
+int	iss_alpha(char c)
+{
+	if ((c >= 'a') && (c <= 'z'))
+		return (1);
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	return (0);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		while (str[i] != '\0' && !iss_alpha(str[i]))
+			i++;
+		if (str[i] != '\0' && ((str[i] >= 'a') && (str[i] <= 'z')))
+			str[i] = ((str[i] - 'a') + 'A');
+		i++;
+		while (str[i] != '\0' && iss_alpha(str[i]))
+		{
+			if ((str[i] >= 'A') && (str[i] <= 'Z'))
+				str[i] = ((str[i] - 'A') + 'a');
+			i++;
+		}
+	}
+	return (str);
+}
+
+int main()
+{
+char str1[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+	char str2[] = " sALuT, ComMenT tu VAS ? 42Mots QUarante-deUX; cinQuantE+ET+un";
+
+	char empty[] = "";
+
+	printf("\n-----\nBEF = %s", str1);
+	printf("\nAFT = %s\n", ft_strcapitalize(str1));
+	printf("\nBEF = %s", str2);
+	printf("\nAFt = %s\n", ft_strcapitalize(str2));
+	printf("\nEmpty = %s\n-----\n", ft_strcapitalize(empty));
+}
