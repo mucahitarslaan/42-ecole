789febc6d0d26b45254175d8fc964e3148a7c0e1
mucahitarslaan <mucahitarslaan@gmail.com>
2022-10-24 02:36:13 +0300

    update

---
+++ EXAM01-study/2-first_word/first_word.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+	int i = 0;
+	
+	while(ac == 2)
+	{
+		while(av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
+		{	write(1,&av[1][i],1);
+			i++;
+		}
+		write(1,"\n",1);
+		return 0;
+	}
+	write(1,"\n",1);
+	return 0;
+}
